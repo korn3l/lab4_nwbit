3e96cdf79c024ef38ad9028e9e18c716d2ab7c4d
Kornel <48766@student.dsw.edu.pl>
2023-04-23 20:17:22 +0200

    utworzenie --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,15 @@
 function menu(){
 	x=$1
 	case "$1" in
-	("--date") echo "Dzisiejsza data: $(date +%d.%m.%Y)"
+	("--date") echo "Dzisiejsza data: $(date +%d.%m.%Y)";
+	("--logs")
+		for i in {1..100}
+			do
+				echo "Plik stworzony przez skrypt.sh --logs" > log${i}.txt
+				echo "log${i}.txt" >> log${i}.txt
+				echo ${i} >> log${i}.txt
+				echo "Dzisiejsza data: $(date +%d.%m.%Y)" >> log${i}.txt
+			done;
 	esac
 }
 menu "$1"
