6e524cd3c3c2b0e51e392c833faab3dc8eb5af08
Kornel <48766@student.dsw.edu.pl>
2023-04-23 20:42:55 +0200

    utworzenie --help wyswietlajacego dostepne opcje

--- skrypt.sh
+++ skrypt.sh
@@ -2,7 +2,7 @@
 function menu(){
 	x=$1
 	case "$1" in
-	("--date") echo "Dzisiejsza data: $(date +%d.%m.%Y)";
+	("--date") echo "Dzisiejsza data: $(date +%d.%m.%Y)";;
 	("--logs")
 		for i in $(seq 1 $2)
 			do
@@ -10,7 +10,10 @@ function menu(){
 				echo "log${i}.txt" >> log${i}.txt
 				echo ${i} >> log${i}.txt
 				echo "Dzisiejsza data: $(date +%d.%m.%Y)" >> log${i}.txt
-			done;
+			done;;
+	("--help")
+		echo "--date -> wyswietli dzisiejsza date"
+		echo "--logs <liczba> -> utworzy podana ilosc plikow log"
 	esac
 }
 menu "$1" "$2"
