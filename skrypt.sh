e706da46a1613d6c0ce0ceb43a624f66748aaac9
Kornel <48766@student.dsw.edu.pl>
2023-04-23 20:47:39 +0200

    dodanie -l -d -h

--- skrypt.sh
+++ skrypt.sh
@@ -2,8 +2,8 @@
 function menu(){
 	x=$1
 	case "$1" in
-	("--date") echo "Dzisiejsza data: $(date +%d.%m.%Y)";;
-	("--logs")
+	("--date"|"-d") echo "Dzisiejsza data: $(date +%d.%m.%Y)";;
+	("--logs"|"-l")
 		for i in $(seq 1 $2)
 			do
 				echo "Plik stworzony przez skrypt.sh --logs" > log${i}.txt
@@ -11,7 +11,7 @@ function menu(){
 				echo ${i} >> log${i}.txt
 				echo "Dzisiejsza data: $(date +%d.%m.%Y)" >> log${i}.txt
 			done;;
-	("--help")
+	("--help"|"-h")
 		echo "--date -> wyswietli dzisiejsza date"
 		echo "--logs <liczba> -> utworzy podana ilosc plikow log"
 	esac
